a1c7726580f42ebe66d3183690073aae0e0a2daa
h-a-graham <hugh.a.graham@gmail.com>
2019-09-27 11:10:37 +0100

    Some more changes to the process pipeline - mainly to process the data in chunks (opted for 1 year) rather than days to reduce the number of times multiple cores need to be started up. Hopefully this should make things a fair bit faster??

--- Nimrod_Process_Pipe.py
+++ Nimrod_Process_Pipe.py
@@ -14,15 +14,21 @@ from functools import partial
 from datetime import datetime
 import time
 
-
+# def main():
 def main(ddirec, edirec):
     startTime = datetime.now()
     print('start time = {0}'.format(startTime))
 
-    # ddir=os.path.abspath("D:/MetOfficeRadar_Data/UK_1km_Rain_Radar/")
+    # ddir=os.path.abspath("D:/MetOfficeRadar_Data/UK_1km_Rain_Radar")
     # ddirec = os.path.abspath("D:/MetOfficeRadar_Data/Data/")
+    # ddirec = os.path.abspath("D:/MetOfficeRadar_Data/Data/2_Day_Test_Ins")
+    # edirec = os.path.abspath("D:/MetOfficeRadar_Data/Data/2_Day_Test_Out")
     # ddirec = os.path.abspath("C:/HG_Projects/SideProjects/Radar_Test_Data")
     # edirec = os.path.join(ddirec, 'Extracted2')
+
+
+    if not os.path.isdir(edirec):
+        os.mkdir(edirec)
 
     unzip_and_convert(ddirec, edirec)
 
@@ -36,15 +42,19 @@ def main(ddirec, edirec):
 
 
 def unzip_and_convert(ddir, edir):
-    if not os.path.isdir(edir):
-        os.mkdir(edir)
 
     print("extracting files begins...")
 
-    for name in tqdm(glob.glob(os.path.join(ddir, "*_1km-composite.dat.gz.tar"))):
-        # print('extracting file: {0}'.format(name))
+    all_files = glob.glob(os.path.join(ddir, "*_1km-composite.dat.gz.tar"))
 
-        shutil.unpack_archive(filename=name, extract_dir=edir)
+    n = 365 #number of days per chunk
+
+    all_files = [all_files[i:i + n] for i in range(0, len(all_files), n)]
+
+    for chunk in tqdm(all_files):
+        # print('extracting file: {0}'.format(name))
+        for name in chunk:
+            shutil.unpack_archive(filename=name, extract_dir=edir)
 
         for name in glob.glob(os.path.join(edir, "*_1km-composite.dat.gz")):
             # print('decompressing file: {0}'.format(name))
@@ -93,8 +103,10 @@ def convert_dat_to_asc(outdir, file_list):
 
         os.remove(asc_name)
         os.remove(name)
+    return()
 
 def paralellProcess(ras_folder):
+    sys.stdout = open(os.devnull, 'w')  # suppress printing
     file_list = []
 
     for name in glob.glob(os.path.join(ras_folder, "*_1km-composite.dat")):
@@ -119,6 +131,9 @@ def paralellProcess(ras_folder):
     pool.close()
     pool.join()
 
+    sys.stdout = sys.__stdout__  # enable printing
+
 if __name__ == "__main__":
+    # main()
     main(sys.argv[1],
          sys.argv[2])
